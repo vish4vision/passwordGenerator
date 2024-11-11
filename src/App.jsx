4551dd315c07cc206ef8e1a221cc7e4898f1fff5
vish4vision <vish4vision@gmail.com>
2024-11-11 14:18:27 +0530

    Button changes and bg changes

--- src/App.jsx
+++ src/App.jsx
@@ -37,8 +37,8 @@ function App() {
   }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
     
-    <div className="w-full max-w-lg mx-auto shadow-md rounded-lg px-10 py-6 my-12 bg-purple-900 text-white">
-      <h1 className='text-white text-center my-2 mb-8 text-3xl drop-shadow-2xl'>Password generator</h1>
+    <div className="bg-gradient-to-br from-slate-800 via-purple-900 to-slate-900 w-full max-w-lg mx-auto shadow-md rounded-lg px-10 py-12 my-12 bg-purple-900 text-white border-2 border-slate-900">
+      <h1 className='text-white text-center mb-10 text-3xl drop-shadow-2xl'>Password Generator</h1>
     <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
             type="text"
@@ -48,13 +48,16 @@ function App() {
             readOnly
             ref={passwordRef}
         />
-        <button
-        onClick={copyPasswordToClipboard}
-        className='outline-none bg-orange-600 text-white px-3 py-0.5 shrink-0'
-        >copy</button>
+ 
+        <button 
+onClick={copyPasswordToClipboard}
+className="relative 
+outline-none bg-yellow-500
+before:ease relative overflow-hidden text-slate-900 shadow-2xl transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-orange-500 before:duration-300 hover:text-white hover:shadow-orange-500 hover:before:h-64 hover:before:-translate-y-32 px-5 py-0.5 shrink-0">
+<span className="relative z-10">COPY</span></button>
         
     </div>
-    <div className='flex text-sm gap-x-4'>
+    <div className='flex text-sm gap-x-4 mt-8'>
       <div className='flex items-center gap-x-2'>
         <input 
         type="range"
